f20e02e74da3d8be1bffb230c63e0a9099a36ea1
murilo costa bittencourt <murilocb1276@gmail.com>
2025-07-22 22:05:19 -0300

    feat: ultima cotação

--- src/controller/yfinance_controller.py
+++ src/controller/yfinance_controller.py
@@ -20,3 +20,19 @@ def get_history_max(ticker: str):
         return history
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/yfinance/latest-price/{ticker}")
+def get_latest_price(ticker: str):
+    """
+    Obtém o preço mais recente de um ticker usando o serviço YFinance.
+    :param ticker: O ticker da ação ou ativo financeiro.
+    :return: Preço mais recente ou None se não encontrado.
+    """
+    try:
+        log.info(f"ticker {ticker}")
+        latest_price = yfinance_service.get_latest_price(ticker)
+        if latest_price is None:
+            raise HTTPException(status_code=404, detail=f"Preço mais recente para o ticker {ticker} não encontrado.")
+        return {"ticker": ticker, "latest_price": latest_price}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- src/services/yfinance_service.py
+++ src/services/yfinance_service.py
@@ -1,10 +1,15 @@
 import yfinance as yf
+
+from src.constants.TimeConstants import ONE_DAY, THIRTY_MINUTES_SECONDS
 from src.domain.mongo_data import MongoData
 from src.services.mongo_service import MongoService
 from datetime import date, datetime
 import logging as log
 
-HISTORY_COLLECTION = 'yfinance-history'
+from src.services.redis_service import RedisService
+
+HISTORY_COLLECTION_MDB = 'yfinance-history'
+LATEST_PRICE_COLLECTION_RD = 'yfinance-latest-price'
 
 class YFinanceService:
     """
@@ -13,17 +18,18 @@ class YFinanceService:
 
     def __init__(self):
         self.mongo_service = MongoService()
+        self.redis_service = RedisService()
 
     def get_history_max(self, ticker: str):
-        history = self.mongo_service.find_one(HISTORY_COLLECTION, {'_id': ticker})
-        if self.is_expired(history, True, 1):
+        history = self.mongo_service.find_one(HISTORY_COLLECTION_MDB, {'_id': ticker})
+        if self.is_expired(history, True, ONE_DAY):
             log.info(f"Data for {ticker} expired or not found, fetching from Yahoo Finance")
             yf_history = yf.Ticker(ticker).history(period="max", interval="1d")
             if not yf_history.empty:
                 history_data = yf_history.reset_index().to_dict(orient='records')
                 mongo_data = MongoData(_id=ticker, data=history_data).to_dict()
-                self.mongo_service.delete(HISTORY_COLLECTION, {'_id': ticker})
-                self.mongo_service.save(HISTORY_COLLECTION, mongo_data)
+                self.mongo_service.delete(HISTORY_COLLECTION_MDB, {'_id': ticker})
+                self.mongo_service.save(HISTORY_COLLECTION_MDB, mongo_data)
                 log.info(f"Data for {ticker} saved to MongoDB")
                 return mongo_data['data']
             else:
@@ -31,7 +37,26 @@ class YFinanceService:
                 return None
         return history['data'] if history['data'] else None
 
-    def is_expired(self, data: dict, only_days: bool = True, lifetime: int = 3600) -> bool:
+    def get_latest_price(self, ticker: str):
+        "Retorna a cotação de um ticker, armazenando em Redis durante 30minutos."
+        key = LATEST_PRICE_COLLECTION_RD + ticker
+        latest_price = self.redis_service.find(key)
+        if latest_price is None:
+            log.info(f"Latest price for {ticker} expired or not found, fetching from Yahoo Finance")
+            yf_ticker = yf.Ticker(ticker)
+            price = yf_ticker.fast_info['lastPrice']
+            if price is not None or price != 0:
+                self.redis_service.save(key, str(price), THIRTY_MINUTES_SECONDS)
+                log.info(f"Latest price for {ticker} saved to Redis")
+                return price
+            else:
+                log.error(f"No latest price found for ticker {ticker}")
+                return None
+        log.info(f"Latest price for {ticker} retrieved from Redis")
+        return float(latest_price)
+
+    @staticmethod
+    def is_expired(data: dict, only_days: bool = True, lifetime: int = 3600) -> bool:
         if not data or 'created_at' not in data:
             log.error("Data not provided or missing 'created_at' attribute for expiration check.")
             return True
